a17ee0be99c6ed0b31c94da3915e638c3c6978c1
igor <igor.honhoff@gmail.com>
2021-03-20 22:23:55 +0200

    getting xdebug to work with the new setup

---
+++ plugin/plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Test Plugin
+Description: DevContainer Sample Plugin
+Version: 1.0
+Author: You
+License: GPLv2 or later
+*/
+
+add_action( 'init', function () {
+  echo 'Plugin installed!';
+}, 10, 0 );
